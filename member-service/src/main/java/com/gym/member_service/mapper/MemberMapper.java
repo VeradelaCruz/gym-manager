cb798cc3c7ebf588bac9416cd3b9eab4012a23ea
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-04 19:27:24 +0100

    Add: create member enpoint in service and create MemberMapper

---
+++ member-service/src/main/java/com/gym/member_service/mapper/MemberMapper.java
@@ -0,0 +1,24 @@
+package com.gym.member_service.mapper;
+
+import com.gym.member_service.dtos.MemberDTO;
+import com.gym.member_service.dtos.MemberRequest;
+import com.gym.member_service.models.Member;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+
+    // Convierte de entidad a dto
+     MemberDTO toDto(Member member);
+
+     //Convierte de dto a entidad
+     Member toEntity(MemberRequest memberRequest);
+
+     //Upgrades parciales
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromDto(MemberRequest dto, @MappingTarget Member entity);
+
+}
--- member-service/src/main/java/com/gym/member_service/service/MemberService.java
+++ member-service/src/main/java/com/gym/member_service/service/MemberService.java
@@ -1,5 +1,9 @@
 package com.gym.member_service.service;
 
+import com.gym.member_service.dtos.MemberDTO;
+import com.gym.member_service.dtos.MemberRequest;
+import com.gym.member_service.mapper.MemberMapper;
+import com.gym.member_service.models.Member;
 import com.gym.member_service.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,5 +11,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberService {
     @Autowired
-    MemberRepository memberRepository;
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private MemberMapper mapper;
+
+    /// ----CRUD OPERATIONS---
+    public MemberDTO createMember(MemberRequest memberRequest){
+        Member member= mapper.toEntity(memberRequest);
+        memberRepository.save(member);
+        return mapper.toDto(member);
+    }
 }
