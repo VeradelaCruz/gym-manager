9e6ca876ba3c14ae196f438f323caeb47fec1432
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-07 17:43:19 +0100

    Add: TrainerUpdateRequest dto for endpoint

---
+++ trainer-service/src/main/java/com/gym/trainer_service/dtos/TrainerUpdateRequest.java
@@ -0,0 +1,25 @@
+package com.gym.trainer_service.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TrainerUpdateRequest {
+
+    String idTrainer;
+    private String name;
+    private String lastName;
+
+    @Indexed(unique = true)
+    @Email(message = "Email should be valid")
+    private String email;
+    private String specialty;
+
+}
