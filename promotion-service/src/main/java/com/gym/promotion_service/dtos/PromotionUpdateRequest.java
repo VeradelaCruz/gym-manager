7252df1219ad8980e086616eeb4186d12171065e
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-11 19:25:01 +0100

    Fix: PromotionUpdateRequest in promotion-service

--- promotion-service/src/main/java/com/gym/promotion_service/dtos/PromotionUpdateRequest.java
+++ promotion-service/src/main/java/com/gym/promotion_service/dtos/PromotionUpdateRequest.java
@@ -6,25 +6,20 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Data
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class PromotionUpdateRequest {
-    private String idPromotion;
 
     private String name;
+
     @Min(value = 0, message = "Discount cannot be less than 0%")
     @Max(value = 100, message = "Discount cannot exceed 100%")
     private Long discountPercentage;
 
-    @FutureOrPresent(message = "Start date must be today or in the future")
     private LocalDate startDate;
-
-    @Future(message = "End date must be in the future")
     private LocalDate endDate;
-    
+
     private Boolean appliesToMembershipType;
 
     @AssertTrue(message = "End date must be after start date")
@@ -33,3 +28,4 @@ public class PromotionUpdateRequest {
         return endDate.isAfter(startDate);
     }
 }
+
