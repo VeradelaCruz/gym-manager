cf89552b1db5118f096c4d1911bb05ded5942de7
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-18 19:26:06 +0100

    add(member-service): findByMembership endpoint

--- member-service/src/main/java/com/gym/member_service/service/MemberService.java
+++ member-service/src/main/java/com/gym/member_service/service/MemberService.java
@@ -5,6 +5,7 @@ import com.gym.member_service.dtos.MemberRequest;
 import com.gym.member_service.dtos.MemberUpdateDTO;
 import com.gym.member_service.enums.MembershipType;
 import com.gym.member_service.exception.MemberNotFound;
+import com.gym.member_service.exception.MembershipNotFound;
 import com.gym.member_service.mapper.MemberMapper;
 import com.gym.member_service.models.Member;
 import com.gym.member_service.repository.MemberRepository;
@@ -68,6 +69,7 @@ public class MemberService {
     //Get by membership
     public MemberDTO findByMembership(MembershipType membershipType){
         Member member= memberRepository.findByMembershipType(membershipType)
-                .orElseThrow()
+                .orElseThrow(()-> new MembershipNotFound(membershipType));
+        return mapper.toDto(member);
     }
 }
