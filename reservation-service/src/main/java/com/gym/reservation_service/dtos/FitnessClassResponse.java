fb4e6f8cfaab83e2e46a11ba4e6daf48c4b229bd
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-22 09:02:55 +0100

    feat(reservation-service): add FitnessClassResponse as dto for feign

---
+++ reservation-service/src/main/java/com/gym/reservation_service/dtos/FitnessClassResponse.java
@@ -0,0 +1,31 @@
+package com.gym.reservation_service.dtos;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FitnessClassResponse {
+    private String idClass;
+    private String name;
+    private String trainer;
+
+    @Min(value = 1, message = "There must be at least 1 participant")
+    private Integer maxParticipants;
+    @FutureOrPresent(message = "Schedule must be present or future")
+    private LocalDateTime scheduleDateTime;
+    @Min(value = 1, message = "Duration must be at least 1 minute")
+    private Long durationMinutes;
+    @DecimalMin(value = "0.0", inclusive = true, message = "Price cannot be negative")
+    private Double price;
+    private boolean active;
+}
--- reservation-service/src/main/java/com/gym/reservation_service/feign/ClassClient.java
+++ reservation-service/src/main/java/com/gym/reservation_service/feign/ClassClient.java
@@ -1,5 +1,6 @@
 package com.gym.reservation_service.feign;
 
+import com.gym.reservation_service.dtos.FitnessClassResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
