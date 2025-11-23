8e702aea5f7b518416dade3f5c43d2c94e8d2774
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-23 07:44:42 +0100

    feat(member-service):  change enpoint  for findByMember

--- member-service/src/main/java/com/gym/member_service/feign/PaymentClient.java
+++ member-service/src/main/java/com/gym/member_service/feign/PaymentClient.java
@@ -4,11 +4,12 @@ import com.gym.member_service.dtos.PaymentDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @FeignClient(name = "payment-client")
 public interface PaymentClient {
-    @GetMapping("/payment/all")
-    ResponseEntity<List<PaymentDTO>> getAll();
+    @GetMapping("/byMemberId/{member}")
+    ResponseEntity<List<PaymentDTO>> getByMemberId(@PathVariable String member);
 }
