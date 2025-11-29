cff452fcb199795ce93b3425f62c016e5ee9a6a2
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-29 17:15:23 +0100

    feat(promotion-service): configure PromotionUsedConsumer

--- promotion-service/src/main/java/com/gym/promotion_service/consumer/PromotionUsedConsumer.java
+++ promotion-service/src/main/java/com/gym/promotion_service/consumer/PromotionUsedConsumer.java
@@ -1,7 +1,10 @@
 package com.gym.promotion_service.consumer;
 
+import com.gym.payment_service.events.PromotionUsedEvent;
+import com.gym.promotion_service.models.PromotionUsage;
 import com.gym.promotion_service.repository.PromotionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +13,27 @@ public class PromotionUsedConsumer {
 
     private final PromotionRepository promotionRepository;
 
+    @KafkaListener(topics = "promotion-used-topic", groupId = "promotion-group")
+    public void consumePromotionUsedEvent(PromotionUsedEvent event) {
+
+        System.out.println("📩 Promotion-service recibió evento: " + event);
+
+        promotionRepository.findById(event.getPromotionId())
+                .ifPresent(promo -> {
+
+                    // Agregar nuevo uso al historial
+                    promo.getUsedHistory().add(
+                            new PromotionUsage(
+                                    event.getMemberId(),
+                                    event.getPaymentId(),
+                                    event.getDateUsed()
+                            )
+                    );
+
+                    promotionRepository.save(promo);
+
+                    System.out.println("🔥 Promoción " + promo.getIdPromotion() +
+                            " actualizada. Total usos: " + promo.getUsedHistory().size());
+                });
+    }
 }
