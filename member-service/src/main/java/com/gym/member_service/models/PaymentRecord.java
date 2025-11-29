2b376e9b2af6357df2da5a7a046e28d76af808a5
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-11-29 06:32:26 +0100

    feat(member-service): add PaymentRecord embeddable class

---
+++ member-service/src/main/java/com/gym/member_service/models/PaymentRecord.java
@@ -0,0 +1,20 @@
+package com.gym.member_service.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+//Clase embebible donde guardamos el historial de pago de cada miembro:
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentRecord {
+    private String paymentId;
+    private Double amount;
+    private LocalDateTime paymentDate;
+    private String promotionId;
+    private Double discountPercentage;
+}
