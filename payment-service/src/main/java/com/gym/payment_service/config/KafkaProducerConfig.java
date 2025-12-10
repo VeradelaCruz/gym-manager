2c2b8f96308019210acf9d3d33b4f949cbb1853e
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-12-10 07:53:28 +0100

    feat(payment-service): add KafkaProducerConfig

---
+++ payment-service/src/main/java/com/gym/payment_service/config/KafkaProducerConfig.java
@@ -0,0 +1,49 @@
+package com.gym.payment_service.config;
+
+import com.gym.payment_service.events.PaymentCreatedEvent;
+import com.gym.payment_service.events.PromotionUsedEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    // Producer para PaymentCreatedEvent
+    @Bean
+    public ProducerFactory<String, PaymentCreatedEvent> paymentProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PaymentCreatedEvent> paymentKafkaTemplate() {
+        return new KafkaTemplate<>(paymentProducerFactory());
+    }
+
+    // Producer para PromotionUsedEvent
+    @Bean
+    public ProducerFactory<String, PromotionUsedEvent> promoProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PromotionUsedEvent> promoKafkaTemplate() {
+        return new KafkaTemplate<>(promoProducerFactory());
+    }
+}
