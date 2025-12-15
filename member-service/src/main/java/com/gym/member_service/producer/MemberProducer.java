0a7c555c97a50bab04bea65da8a3f12c90dfb4aa
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-12-15 07:57:23 +0100

    feat(member-service): add kafkaProducerConfig

--- member-service/src/main/java/com/gym/member_service/producer/MemberProducer.java
+++ member-service/src/main/java/com/gym/member_service/producer/MemberProducer.java
@@ -2,16 +2,16 @@ package com.gym.member_service.producer;
 
 import com.gym.member_service.events.NewMemberEvent;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberProducer {
-    private final KafkaTemplate<String, NewMemberEvent> memberEventKafkaTemplate;
+    private final KafkaTemplate<String, NewMemberEvent> kafkaTemplate;
 
     public void sendNewMemberEvent(NewMemberEvent event) {
-        memberEventKafkaTemplate.send("member-created-topic", event);
+        kafkaTemplate.send("member-created-topic", event);
     }
-
 }
