6247340bc21a2ee3c7aca542a8de9e8f0cc5e003
VeradelaCruz <maidanabru@yahoo.com.ar>
2026-02-26 17:36:03 +0100

    Changer url for apigateway in class-service

--- class-service/src/main/java/com/gym/class_service/controller/ClassController.java
+++ class-service/src/main/java/com/gym/class_service/controller/ClassController.java
@@ -14,7 +14,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/class")
+@RequestMapping("/classes")
 public class ClassController {
     @Autowired
     private ClassService classService;
