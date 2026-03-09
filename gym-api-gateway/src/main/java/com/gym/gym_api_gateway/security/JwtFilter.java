e23491387e97a04092fcb6340cbda4a804053ef4
VeradelaCruz <maidanabru@yahoo.com.ar>
2026-03-09 18:42:27 +0100

    Add comments to JwtService in auth-service

--- gym-api-gateway/src/main/java/com/gym/gym_api_gateway/security/JwtFilter.java
+++ gym-api-gateway/src/main/java/com/gym/gym_api_gateway/security/JwtFilter.java
@@ -8,13 +8,25 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+//JwtFilter intercepta todas las requests que llegan al gateway
+//Hace lo siguiente:
+//Extrae el Authorization header
+//Saca el token
+//Llama a jwtService.validateToken(token)
+//Comprueba firma con secret
+//Comprueba expiración
 @Component
 @RequiredArgsConstructor
+//GatewayFilter → interfaz de Spring Cloud Gateway que permite
+// interceptar todas las requests antes de enviarlas a los microservicios.
 public class JwtFilter implements GatewayFilter {
 
     private final JwtService jwtService;
 
     @Override
+    //ServerWebExchange exchange → representa la petición y la respuesta HTTP
+    //GatewayFilterChain chain → representa la cadena de filtros que la request
+    // seguirá si pasa la validación
     public Mono<Void> filter(ServerWebExchange exchange,
                              GatewayFilterChain chain) {
 
